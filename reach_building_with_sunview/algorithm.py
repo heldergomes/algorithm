6d801f08d7bf48860c153a1bea2a2a9d6e60b6a5
Helder Ardachnikoff Gomes <tomate@MacBook-Pro-de-Helder.local>
2022-09-04 19:05:21 -0700

    add algorithm able to find buildings with sunview

---
+++ reach_building_with_sunview/algorithm.py
@@ -0,0 +1,23 @@
+"""
+Basically the idea was read the array from the end to the begin and save the latest
+big building found, so that way was able to develop the algorithm with O(n)
+"""
+
+
+def reach_building_with_sunview(param):
+    index = len(param) - 1
+    biggest_building = -1
+    sun_view_counter = 0
+
+    for _ in range(len(param)):
+        if param[index] > biggest_building:
+            biggest_building = param[index]
+            sun_view_counter += 1
+        index -= 1
+
+    return sun_view_counter
+
+
+if __name__ == '__main__':
+    building_list = [12, 11, 8, 13, 6, 9]
+    print(reach_building_with_sunview(building_list))
